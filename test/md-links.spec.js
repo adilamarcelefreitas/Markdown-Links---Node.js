d35153060ba80c73db5fc6c20852f5105266df21
unknown <adilamarcele@hotmail.com>
2023-10-23 15:04:30 -0300

    Test updates

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,18 +1,14 @@
-const { mdLinks, validate, normalizeURL, readMarkdownFile } = require('../src/mdLinks.js');
+const { mdLinks, validate, normalizeURL, readFiles } = require('../src/mdLinks.js');
 
 
+//function normalizeURL
 test('normalizeURL should add "http://" to the URL if it does not start with "http"', () => {
   const url = 'example.com';
   const normalized = normalizeURL(url);
   expect(normalized).toBe('http://example.com');
 });
 
-test('normalizeURL should not modify the URL if it starts with "http"', () => {
-  const url = 'http://example.com';
-  const normalized = normalizeURL(url);
-  expect(normalized).toBe('http://example.com');
-});
-
+//function validate
 test('validate should return a PASS status for a valid link', () => {
   const link = { href: 'https://example.com' };
   const responseMock = { statusCode: 200 };
@@ -33,7 +29,7 @@ test('validate should return a PASS status for a valid link', () => {
   });
 });
 
-test('validate should return a FAIL status for an invalid link', () => {
+test('validate should return a ERROR status for an invalid link', () => {
   const link = { href: 'https://example.com' };
   const responseMock = { statusCode: 404 };
   const requestMock = jest.fn().mockReturnValue({
@@ -48,32 +44,46 @@ test('validate should return a FAIL status for an invalid link', () => {
   jest.spyOn(require('https'), 'request').mockImplementation(requestMock);
 
   return validate(link).then((result) => {
-    expect(result.ok).toBe('FAIL');
+    expect(result.ok).toBe('ERROR');
     expect(result.status).toBe(404);
   });
 });
 
+//function readFiles
+test('readFiles should read and resolve the content of an existing .md file', () => {
+  const filePath = path.join('text.md'); // Path to an existing .md file
+  return readFiles(filePath)
+    .then((data) => {
+      // Assuming the test.md file contains some content
+      expect(data).toBeTruthy();
+    });
+});
 
+test('readFiles should reject with an error for a non-existing .md file', () => {
+  const filePath = 'text.md';
+  return expect(readFiles(filePath)).rejects.toThrow('Error! File not defined');
+});
+
+//function mdLinks
 test('mdLinks should return an array of links', async () => {
-  const filePath = 'test.md';
+  const filePath = 'text.md';
   const options = {};
   const result = await mdLinks(filePath, options);
   expect(Array.isArray(result)).toBe(true);
-  
 });
 
 test('mdLinks should validate links if options.validate is true', async () => {
-  const filePath = 'test.md';
+  const filePath = 'text.md';
   const options = { validate: true };
   const result = await mdLinks(filePath, options);
   expect(Array.isArray(result)).toBe(true);
   result.forEach((link) => {
-    expect(link.ok === 'ok' || link.ok === 'fail').toBe(true);
+    expect(link.ok === 'PASS' || link.ok === 'ERROR').toBe(true);
   });
 });
 
 test('mdLinks should return link statistics if options.stats is true', async () => {
-  const filePath = 'test.md';
+  const filePath = 'text.md';
   const options = { stats: true };
   const result = await mdLinks(filePath, options);
   expect(result.total).toBeDefined();
